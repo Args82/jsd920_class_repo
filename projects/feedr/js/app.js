d6772d362906453620c9553f454d9d3ddaf2b065
ATA web dev <alexandra.todd@gmail.com>
2016-10-26 19:18:39 -0400

    pulls data

--- projects/feedr/js/app.js
+++ projects/feedr/js/app.js
@@ -2,17 +2,47 @@
   Please add all Javascript code to this file.
 */
 
+$(document).ready(function () {
+ // var apiKey = 'b1950c77e51644e283ac7f6326b45597';
+  var newsUrl = 'https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=b1950c77e51644e283ac7f6326b45597';
 
-var articleObj = { 
-    article: "",
-    section: ""
-                // }$('#script ID on html')
-// var newVariable = $('#title-template').html();
-var template = Handlebars.compile( $('#title-template').html() ); 
+  
+    $.get(newsUrl)
+    .fail(function (xhr) {
+      console.log(xhr);
+    })
+    .done(function (response) {
+      console.log(response);
+    });
 
-var articleTemplate = template(articleObj);
-$('.article').append(articleTemplate);
-=======
+  
+
+    // $('.article')
+    //   .append('<p>'a'</p>')
+    
+    // $('.featuredImage')
+    //   .append('<p>'b' </p>')
+
+    // $('.articleContent')
+    //   .append('<p>'c' </p>')
+
+//   	});
+  
+
+
+
+  		
+  
+
+
+
+
+
+
+
+
+
+
 //step1: grab the handlebars html template
 
 //step2: compile the template using Handlebars.compile()
@@ -20,3 +50,18 @@ $('.article').append(articleTemplate);
 //step3: pass compile the obj
 
 //step4: append the obj(s) to the html element
+// $('.article') & $('.articleContent')
+
+
+// var template = Handlebars.compile($('#article-template').html());
+
+// var articleObj = { 
+//     article: "",
+//     section: ""
+// };
+// 	$('.article').append(template(articleObj))
+
+
+
+});
+
