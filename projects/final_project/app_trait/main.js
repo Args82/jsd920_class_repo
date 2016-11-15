b3297b6e5768e5e787c4bfea56fd9f257624f47f
ATA web dev <alexandra.todd@gmail.com>
2016-11-15 18:28:47 -0500

    updates

--- projects/final_project/app_trait/main.js
+++ projects/final_project/app_trait/main.js
@@ -1,38 +1,55 @@
 $(document).ready(function() {
     /* body... */
+    //login/sign up?
+    //currently slides repeat themselves and don't stop after 56
+    //need to grab data after slide 56
+    //find career matches
+    //display results
+    //** need to clear results for next client
 
-    //pulic 	399c11fc4bb447b9d67d175d88
-    //secret  	508f7a24948e461475c8858c8a
 
 
-    var assessmentId = 'd88486bb-3357-4038-857e-38e7f7560258';
+    //pulic     399c11fc4bb447b9d67d175d88
+    //secret    508f7a24948e461475c8858c8a
+
+    //     curl https://api-sandbox.traitify.com/v1/assessments \
+    // -H "Content-Type: application/json" \
+    // -u 508f7a24948e461475c8858c8a:x \
+    // -d '{"deck_id": "career-deck"}'
+    // {"id":"943f952d-3f7e-45a8-8716-1e2e4d930bcf","deck_id":"career-deck","completed_at":null,"created_at":1479145851948,"locale_key":"en-US","profile_id":"201e1fe7-c4d2-444f-b952-cf88a8794f6e"}ALEXANDRAs-MacBook-Air:~ ATA$ 
+
+    //send request for assessment id to traitify
+    var assessmentId = '943f952d-3f7e-45a8-8716-1e2e4d930bcf';
 
     Traitify.setPublicKey("399c11fc4bb447b9d67d175d88");
     Traitify.setHost("https://api-sandbox.traitify.com");
     Traitify.setVersion("v1");
 
-    function updateProgress(position){
-    	var percentage = Math.ceil(position / parseInt($('#total-slides').text()) * 100);
 
-    	$('#current-slide-position').text(position);
-    	$('#slide-percentage').text(percentage);
-    	$('#percentage-bar').css('width', percentage + '%');
+    //progress bar
+    function updateProgress(position) {
+        var percentage = Math.ceil(position / parseInt($('#total-slides').text()) * 100);
+
+        $('#current-slide-position').text(position);
+        $('#slide-percentage').text(percentage);
+        $('#percentage-bar').css('width', percentage + '%');
     }
 
     // 1. hard code the career deck id
-    var deck_id = 'super-hero';
+    var deck_id = 'career_deck'; //do I need deck id?
 
     function slideUpdate(assessment_id, slide_id, time_taken, response) {
         var url = '/assessments/' + assessment_id + '/slides/' + slide_id;
         var params = {
             id: assessment_id,
-            response: response,
-            time_taken: time_taken
+            response: response, //true/false 
+            time_taken: time_taken //need to grab time taken
         }
         return Traitify.put(url, params);
     }
 
-
+    //at slide 56 grab data and display results
+    //NOT ME button
     $('#slides-target').on('click', '.notMeBtn', function(event) {
         event.preventDefault();
         var slide_id = $(this).data('slide_id');
@@ -40,28 +57,44 @@ $(document).ready(function() {
         var position = $(this).data('position');
 
         // 1. send this current assessment id and the current slide id with the "not me" value to Traitify
-        slideUpdate(assessment_id, slide_id, 2000, false).then(function(response){
-        	// 2. go to the next slide
-        	updateProgress(position+1);
-	        $('#trait-carousel').carousel('next');
+        slideUpdate(assessment_id, slide_id, 2000, false).then(function(response) {
+            // 2. go to the next slide
+            updateProgress(position);
+            $('#trait-carousel').carousel('next');
         });
     });
 
-   $('#slides-target').on('click', '.meBtn', function(event) {
+    //ME button
+    $('#slides-target').on('click', '.meBtn', function(event) {
         event.preventDefault();
         var slide_id = $(this).data('slide_id');
         var assessment_id = $(this).data('assessment_id');
         var position = $(this).data('position');
 
         // 1. send this current assessment id and the current slide id with the "not me" value to Traitify
-        slideUpdate(assessment_id, slide_id, 2000, true).then(function(response){
-        	// 2. go to the next slide
-        	updateProgress(position+1);
-	        $('#trait-carousel').carousel('next');
+        slideUpdate(assessment_id, slide_id, 2000, true).then(function(response) {
+            // 2. go to the next slide
+            updateProgress(position + 1);
+            $('#trait-carousel').carousel('next');
         });
     });
-    
 
+    //don't know if this works
+    function getResults(assessment_id, blend, types, traits, career_matches) {
+        var url = /assessments/ + assessment_id + data;
+        var params = {
+            id: assessment_id,
+            blend: blend,
+            types: types,
+            traits: traits,
+            career_matches: career_matches
+        }
+
+        return traitify.get(url, data);
+
+
+        /* body... */
+    }
     //* this grabs all 6 decks
     // Traitify.get("/decks", function(data) {
     //         console.log(data, data)
@@ -69,12 +102,12 @@ $(document).ready(function() {
 
 
 
-    // 	POST
-    // 	Authorization: Basic {your_secret_key}:x
-    // 	https://api.traitify.com/v1/assessments
-    // 	{
-    // 	  'deck_id': 'career-deck'
-    // 	}
+    //  POST
+    //  Authorization: Basic {your_secret_key}:x
+    //  https://api.traitify.com/v1/assessments
+    //  {
+    //    'deck_id': 'career-deck'
+    //  }
     //skip for now
     // Traitify.put('/assessments', {
     //     deck_id: deck_id
@@ -106,33 +139,33 @@ $(document).ready(function() {
 
 
 
-    // 	[
-    // 	    {
-    // 	        "id": "b93af357-0cd7-494e-b436-c67313c0fab6",
-    // 	        "position": 1,
-    // 	        "caption": "Using a Microscope ",
-    // 	        "image_desktop": "https://traitify-api.s3.amazonaws.com/slides/b93af357-0cd7-494e-b436-c67313c0fab6/desktop",
-    // 	        "image_desktop_retina": "https://traitify-api.s3.amazonaws.com/slides/b93af357-0cd7-494e-b436-c67313c0fab6/desktop_retina",
-    // 	        "response": null,
-    // 	        "time_taken": null,
-    // 	        "completed_at": null,
-    // 	        "created_at": 1414091516995,
-    // 	        "focus_x": 62,
-    // 	        "focus_y": 47
-    // 	    },
-    // 	]
+    //  [
+    //      {
+    //          "id": "b93af357-0cd7-494e-b436-c67313c0fab6",
+    //          "position": 1,
+    //          "caption": "Using a Microscope ",
+    //          "image_desktop": "https://traitify-api.s3.amazonaws.com/slides/b93af357-0cd7-494e-b436-c67313c0fab6/desktop",
+    //          "image_desktop_retina": "https://traitify-api.s3.amazonaws.com/slides/b93af357-0cd7-494e-b436-c67313c0fab6/desktop_retina",
+    //          "response": null,
+    //          "time_taken": null,
+    //          "completed_at": null,
+    //          "created_at": 1414091516995,
+    //          "focus_x": 62,
+    //          "focus_y": 47
+    //      },
+    //  ]
 
     Traitify.getSlides(assessmentId).then(function(slideObj) {
-		console.log(slideObj);
-		$('#total-slides').text(slideObj.length);
+        console.log(slideObj);
+        $('#total-slides').text(slideObj.length);
         var slideTemplate = $('#slide-template').html();
         var buildSlideHtmlFunction = Handlebars.compile(slideTemplate);
 
         var slides_array = [];
         slideObj.forEach(function(slide) {
             slides_array.push({
-            	id: slide.id,
-            	assessment_id: assessmentId,
+                id: slide.id,
+                assessment_id: assessmentId,
                 image: slide.image_desktop,
                 caption: slide.caption,
                 position: slide.position
@@ -150,23 +183,24 @@ $(document).ready(function() {
     });
 
 
+
     // 4. create a carousel (slides) with the image and 2 buttons
-    // 	button 1 "Me"
-    // 	button 2 "Not me"
-    // 	** if not too hard, show progress bar
+    //  button 1 "Me"
+    //  button 2 "Not me"
+    //  ** if not too hard, show progress bar
 
     // 5. depending on which button the user clicked, we want to make another ajax call to record their response
-    // 	PUT
-    // 		Authorization: Basic {your_public_key}:x
-    // 		https://api.traitify.com/v1/assessments/{assessment_id}/slides/{slide_id}
-    // 		{
-    // 		    "id": "781acd1f-8184-482d-a3d1-e6190d0b7db5",
-    // 		    "response": true,
-    // 		    "time_taken": 1000
-    // 		}
+    //  PUT
+    //     
+    //      https://api.traitify.com/v1/assessments/{assessment_id}/slides/{slide_id}
+    //      {
+    //          "id": "781acd1f-8184-482d-a3d1-e6190d0b7db5",
+    //          "response": true,
+    //          "time_taken": 1000
+    //      }
 
     // 6. at the end make another ajax call to results
-    // 	https://api.traitify.com/v1/assessments/{assessment_id}?data=blend,types,traits,career_matches
+    //  https://api.traitify.com/v1/assessments/{assessment_id}?data=blend,types,traits,career_matches
 
 
     // * /
