58f16637447a2b405eb57eefdf7010e806ba9738
ATA web dev <alexandra.todd@gmail.com>
2016-11-15 18:29:37 -0500

    updates

---
+++ week8/class-15-crud-and-firebase/forum_app_starter_code/js/app.js
@@ -0,0 +1,73 @@
+//CR$(document).ready(function() {
+
+var messageAppReference = firebase.database(); //must use to start firebase
+
+$('#message-form').submit(function(e) {
+    e.preventDefault();
+    var message = $('#message').val();
+    $('#message').val('');
+
+    var messagesReference = messageAppReference.ref('messages')
+    messagesReference.push({
+        msg: message,
+        votes: 1
+    })
+
+    function getMessages() {
+        messageAppReference.ref('messages').on('value', function(res) {
+            $('.message-board').empty()
+            res.forEach(function(msg) {
+                console.log("this is the actual obj: ", msg)
+                var id = msg.key
+                var message = msg.val();
+
+                var messageText = message.msg
+                var votes = message.votes
+                    //create a new li item
+                    //populate that li with the content
+                    //append the li
+                var li = $('<li>')
+                var upVote = $('<i class="fa  fa-thumbs-up pull-right"></i>')
+
+                // put ++ and -- infront of votes to fire when clicked
+
+                upVote.on('click', function() {
+                    updateMessage(id, ++votes)
+                        /* body... */
+                })
+                var downVote = $('<i class="fa  fa-thumbs-down pull-right"></i>')
+                downVote.on('click', function() {
+                    updateMessage(id, --votes)
+                        /* body... */
+                })
+                var remove = $('<i class="fa  fa-trash pull-right"></i>')
+                remove.on('click', function() {
+                    li.empty()
+                        /* body... */
+                })
+
+
+                li.html(messageText)
+                li.append(upVote)
+                li.append(downVote)
+                li.append(remove)
+                li.append('<div class="pull-right">' + votes + '</div>')
+
+                $('.message-board').append(li)
+
+                //fix repeats
+                //refactor
+
+            });
+        });
+
+    }
+
+    function updateMessage(id, votes) {
+        var messagesReference = messageAppReference.ref('messages/' + id)
+        messagesReference.update({ votes: votes })
+    }
+
+    getMessages()
+
+}); //message-form
