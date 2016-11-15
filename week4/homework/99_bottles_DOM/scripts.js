d9fe8f446ef1d2f04ec0aa42bdd28137b0f68058
ATA web dev <alexandra.todd@gmail.com>
2016-11-15 18:30:38 -0500

    nothing added

--- week4/homework/99_bottles_DOM/scripts.js
+++ week4/homework/99_bottles_DOM/scripts.js
@@ -1,19 +1,28 @@
-
-$(function () {
-	
+$(function() {
 
 
+    $('button').click(function() {
 
+    	var lyrics = $('#lyrics').val()
+
+
+
+
+
+
+
+
+
+        for (var i = 99; i > 0; i--)
+            if (i === 2) {
+                $('div').append('<p>' +
+                    i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down and pass it around, " + (i - 1) + " bottle of beer on the wall." + '</p>');
+            } else if (i === 1) {
+            console.log(i + " bottle of beer on the wall, " + i + " bottle of beer. Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.");
+        } else {
+            console.log(i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.");
+        }
+        lyrics.html()
+    })
 
 });
-
-
-
-for (var i = 99; i > 0; i--)
-    if (i === 2) {
-        console.log(i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down and pass it around, " + (i - 1) + " bottle of beer on the wall.");
-    } else if (i === 1) {
-    console.log(i + " bottle of beer on the wall, " + i + " bottle of beer. Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.");
-} else {
-    console.log(i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.");
-}
