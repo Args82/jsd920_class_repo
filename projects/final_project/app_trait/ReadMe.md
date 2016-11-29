9418543c765ccc6be103bdbf3666e8c321a97d70
ATA web dev <alexandra.todd@gmail.com>
2016-11-29 10:54:25 -0500

    final_updates

--- projects/final_project/app_trait/ReadMe.md
+++ projects/final_project/app_trait/ReadMe.md
@@ -1,55 +1,95 @@
 
 
-
-
-pulic     399c11fc4bb447b9d67d175d88
- secret    508f7a24948e461475c8858c8a
-
-      curl https://api-sandbox.traitify.com/v1/assessments \
-     -H "Content-Type: application/json" \
-     -u 508f7a24948e461475c8858c8a:x \
-     -d '{"deck_id": "career-deck"}'
-    // {"id":"943f952d-3f7e-45a8-8716-1e2e4d930bcf","deck_id":"career-deck","completed_at":null,"created_at":1479145851948,"locale_key":"en-US","profile_id":"201e1fe7-c4d2-444f-b952-cf88a8794f6e"}ALEXANDRAs-MacBook-Air:~ ATA$ 
-
-
 # Project Overview
-	An application that gives you career options based on your personality type.
+	An application that finds career matches based on personality type.
 
 ## Project Description
 
-	This app gathers information based on a personality assessment the client takes and then returns their personality type, traits and career matches.
+	Take assessment
+	Returns personality type results
+	Returns career matches that best suit you
+	Connects you to the O*NET database, containing information on hundreds of standardized and occupation-specific descriptors
 
-traitify.com
+	*As of now this app has already been set to a Users results
 
 Use this section to describe their final project and perhaps any links to relevant sites that help convey the concept and\or functionality
+
+	API DOCS
+	https://student12.traitify.com/developer 
+	https://github.com/traitify/traitify-js-widgets
+	https://github.com/traitify/traitify-js-client
+	traitify.com
 
 ## Functional Components
 
 This is a breakdown of the top level functionality for final project
 
-| Component | Priority | Estimated Time | Time Invetsted | Actual Time |
+| Component | Priority | Estimated Time | Actual Time |
 | --- | :---: |  :---: | :---: | :---: |
-| Component 1 | H | 10hrs| 5hrs |  |
+| GET request for assessment| H | 2hrs| 3hrs | 
+| --- | :---: |  :---: | :---: | :---: |
+| PUT request for slide data| H | 2hrs| 3hrs |  
+| --- | :---: |  :---: | :---: | :---: |
+| GET results for personality| H | 2hrs| 1hrs |  
+| --- | :---: |  :---: | :---: | :---: |
+| GET career matches for assessment| H | 2hrs| 5hrs |   
+| --- | :---: |  :---: | :---: | :---: |
+| HTML & CSS| H | 2hrs| 3hrs |  
 
 ## Helper Functions
 Use this section to document all helper functions. These functions should be versatile enough to be reused in other projects
 
 ## Additional Libraries
- Use this section to list all supporting libraries and thier role in the project
+	jQuery
+	Handlebars
+	Bootstrap
+	Traitify Ui
+	Google fonts
 
 ## JSON Data Keys
  Use this section to list specific JSON Data Key mappings.  
+ 	Obtain your authorization keys.
+ 		public     399c11fc4bb447b9d67d175d88
+		secret    xxxxxxxxx8c8a
+	Use code in terminal to get authorization results
+      curl https://api-sandbox.traitify.com/v1/assessments \
+		-H "Content-Type: application/json" \
+		-u 508f7a24948e461475c8858c8a:x \
+		-d '{"deck_id": "career-deck"}'
+    Response should look like
+    	{"id":"17e5bb42-f56f-46b8-aeb3-9f70bd5bd298","deck_id":"career-deck","completed_at":null,"created_at":1480380408773,"locale_key":"en-US","profile_id":"990a4599-412c-4054-9a3a-0ba9e11a9ef7"}
 
 ## jQuery Functional Requirements
  Use this section to list some, but not all, of the jQuery methods discovered while working on this project.
 
+| Function Name | Purpose |
+| --- | :---: | --- | :---: |
+| getSlides	 | GET request for each slide in assessment |
+| --- | :---: | --- | :---: |
+| slideUpdate |	PUT request, keeps track of slide data |
+| --- | :---: | --- | :---: |
+| updateProgress | Displays progress bar |
+| --- | :---: | --- | :---: |
+| lastSlide	| Determines which slide is last |
+| --- | :---: | --- | :---: |
+| getResults | GET Request for personality results |
+| --- | :---: | --- | :---: |
+| showCareers |	GET Request for career matches |
+
+
 ## Change Log
- Use this section to document what changes were made and when.
+ I hate the styling and will continue to update it
 
 ## Issues and Resolutions
  Use this section to list of all issues encountered and their resolution
+	Displaying career-matches
+		need to make two separate functions for career and personality results
 
-#### SAMPLE.....
-**ERROR**: app.js:34 Uncaught SyntaxError: Unexpected identifier                                
-**RESOLUTION**: Missing comma after first object in sources {} object
+	Clearing results
+		need to get a new assessement id for each client
+		need a server side library
+	Handlebars
+		had difficulty setting template up properly and getting it to work
+		became confusing when trying to style
+
 
--- projects/final_project/app_trait/main.js
+++ projects/final_project/app_trait/main.js
@@ -1,8 +1,70 @@
 $(document).ready(function() {
-    var assessmentId = '8b31b590-5a9c-4df8-9765-b2c798e06398';
+    var assessmentId = '17e5bb42-f56f-46b8-aeb3-9f70bd5bd298';
     Traitify.setPublicKey("399c11fc4bb447b9d67d175d88");
     Traitify.setHost("https://api-sandbox.traitify.com");
     Traitify.setVersion("v1");
+
+     //FUNCTION KEEPS TRACK OF DATA RECEIVED FROM EACH SLIDE
+
+    function slideUpdate(assessment_id, slide_id, response) {
+        var url = '/assessments/' + assessment_id + '/slides/' + slide_id;
+        var params = {
+            id: assessment_id,
+            response: response, //true/false 
+            slide_id: slide_id 
+        }
+        return Traitify.put(url, params);
+    
+    //NOT ME button
+  //COLLECTS CURRENT DATA AND SENDS DATA BACK AT CLICK BUTTON FOR EACH SLIDE
+
+    $('#slides-target').on('click', '#notMeBtn', function(event) {
+        event.preventDefault();
+        var slide_id = $(this).data('slide_id');
+        var assessment_id = $(this).data('assessment_id');
+        var position = $(this).data('position');
+      slideUpdate(assessment_id, slide_id, false).then(function(response) {
+
+            if (lastSlide()) {      //HIDES ALL SLIDES AT THE LAST ONE AND REQUESTS RESULTS
+                //addClass hidden
+                $('#ui').addClass('hidden');
+                getResults()
+                showCareers()
+            } else {
+                                //OTHERWISE GO TO NEXT SLIDE
+                updateProgress(position + 1);
+                $('#trait-carousel').carousel('next');
+            }
+        });
+    });
+
+
+    //ME button
+    //COLLECTS CURRENT DATA AND SENDS DATA BACK AT CLICK BUTTON FOR EACH SLIDE
+
+    $('#slides-target').on('click', '#meBtn', function(event) {
+        event.preventDefault();
+        var slide_id = $(this).data('slide_id');
+        var assessment_id = $(this).data('assessment_id');
+        var position = $(this).data('position');
+      slideUpdate(assessment_id, slide_id, true).then(function(response) {
+           
+            if (lastSlide()) {     //HIDES ALL SLIDES AT THE LAST ONE AND REQUESTS RESULTS
+                //addClass hidden
+                $('#ui').addClass('hidden');
+                getResults()
+          
+                showCareers()
+            } else {
+                // OTHERWISE GO TO NEXT SLIDE
+                updateProgress(position + 1);
+                $('#trait-carousel').carousel('next');
+            }
+        });
+    });
+
+
+
     //PROGRESS BAR
     function updateProgress(position) {
         var percentage = Math.ceil(position / parseInt($('#total-slides').text()) * 100);
@@ -10,6 +72,8 @@ $(document).ready(function() {
         $('#slide-percentage').text(percentage);
         $('#percentage-bar').css('width', percentage + '%');
     }
+
+
     //DETERMINES WHEN ON LAST SLIDE
     function lastSlide() {
         if ($('#current-slide-position').text() == $('#total-slides').text()) {
@@ -19,6 +83,8 @@ $(document).ready(function() {
         }
     }
 
+
+    //GRABS PERSONALITY RESULTS FROM THE TRAITIFY API
     function getResults() {
         Traitify.get("/assessments/" + assessmentId + "?data=blend,types,traits").then(function(res) {
             console.log(res)
@@ -28,70 +94,22 @@ $(document).ready(function() {
         })
     }
 
+    //GRABS CAREER MATCHES FROM THE TRAITIFY API
     function showCareers() {
         Traitify.get("/assessments/" + assessmentId + "?data=career_matches").then(function(res) {
             console.log(res)
             traitify = Traitify.ui.load("careers", assessmentId, ".career")
         })
     }
-    //GETS EACH SLIDE 
-    function slideUpdate(assessment_id, slide_id, time_taken, response) {
-        var url = '/assessments/' + assessment_id + '/slides/' + slide_id;
-        var params = {
-            id: assessment_id,
-            response: response, //true/false 
-            time_taken: time_taken,
-            slide_id: slide_id //need to grab time taken
-        }
-        return Traitify.put(url, params);
+   
     }
-    //NOT ME button
-    //COLLECTS AND SENDS DATA BACK AT CLICK BUTTON
-    $('#slides-target').on('click', '#notMeBtn', function(event) {
-        event.preventDefault();
-        var slide_id = $(this).data('slide_id');
-        var assessment_id = $(this).data('assessment_id');
-        var position = $(this).data('position');
-        //COLLECTS EACH SLIDE'S CURRENT DATA AND HIDES ALL SLIDES AT THE LAST ONE AND REQUESTS RESULTS
-        slideUpdate(assessment_id, slide_id, 2000, false).then(function(response) {
-            if (lastSlide()) {
-                //addClass hidden
-                $('#ui').addClass('hidden');
-                getResults()
-                showCareers()
-            } else {
-                // GO TO NEXT SLIDE
-                updateProgress(position + 1);
-                $('#trait-carousel').carousel('next');
-            }
-        });
-    });
-    //ME button
-    //COLLECTS AND SENDS DATA BACK AT CLICK BUTTON
-    $('#slides-target').on('click', '#meBtn', function(event) {
-        event.preventDefault();
-        var slide_id = $(this).data('slide_id');
-        var assessment_id = $(this).data('assessment_id');
-        var position = $(this).data('position');
-        //COLLECTS EACH SLIDE'S CURRENT DATA AND HIDES ALL SLIDES AT THE LAST ONE AND REQUESTS RESULTS
-        slideUpdate(assessment_id, slide_id, 2000, true).then(function(response) {
-            // 2. go to the next slide
-            if (lastSlide()) {
-                //addClass hidden
-                $('#ui').addClass('hidden');
-                getResults()
-                    // showTraits()
-                    // showTypes()
-                showCareers()
-            } else {
-                // GOT TO NEXT SLIDE
-                updateProgress(position + 1);
-                $('#trait-carousel').carousel('next');
-            }
-        });
-    });
+
+
+    //HANDLEBARS
+
+    //TAKES SLIDE PROPERTIES GOES THROUGH ALL SLIDES AND PUSHES INFO INTO THE ARRAY
     Traitify.getSlides(assessmentId).then(function(slideObj) {
-        slideObj = slideObj.slice(0, 3);
+        slideObj = slideObj.slice(0,3);
         $('#total-slides').text(slideObj.length);
         var slideTemplate = $('#slide-template').html();
         var buildSlideHtmlFunction = Handlebars.compile(slideTemplate);
